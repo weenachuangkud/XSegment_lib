6f0b33fe4af8ae1ac9aa9a512c2d78f18ff18c97
Mawin Chuangkud <weena.chuangkud@gmail.com>
2026-04-25 04:13:07 +0700

    Add GPIO manipulation macros for ESP32

--- src/platform/XSeg_ESP32.h
+++ src/platform/XSeg_ESP32.h
@@ -1,1 +1,24 @@
+#pragma once
+#include "driver/gpio.h"
 
+// ESP32 uses SET/CLEAR registers
+// Writing to w1ts sets bits, w1tc clears bits
+// Much faster than digitalWrite
+
+#define XSEG_PIN_HIGH(pin)        \
+    (GPIO.out_w1ts = (1UL << (pin)))
+
+#define XSEG_PIN_LOW(pin)         \
+    (GPIO.out_w1tc = (1UL << (pin)))
+
+#define XSEG_PIN_OUTPUT(pin)      \
+    (GPIO.enable_w1ts = (1UL << (pin)))
+
+#define XSEG_PIN_READ(pin)        \
+    ((GPIO.in >> (pin)) & 0x1)
+
+#define XSEG_PIN_TOGGLE(pin)      \
+    ((GPIO.out >> (pin)) & 0x1) ? XSEG_PIN_LOW(pin) : XSEG_PIN_HIGH(pin)
+
+#define XSEG_PORT_WRITE(port, mask) \
+    (GPIO.out_w1ts = (mask))
