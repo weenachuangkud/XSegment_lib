e203ef0d9ae24c5741af02e1118c72226e34f506
Mawin Chuangkud <weena.chuangkud@gmail.com>
2026-04-25 04:12:29 +0700

    Update XSeg_AVR.h

--- src/platform/XSeg_AVR.h
+++ src/platform/XSeg_AVR.h
@@ -1,1 +1,23 @@
+#pragma once
+#include <avr/io.h>
 
+// Direct PORT register manipulation
+// Works on Uno, Nano, Mega, ATtiny...
+
+#define XSEG_PIN_HIGH(pin)        \
+    (*portOutputRegister(digitalPinToPort(pin)) |= digitalPinToBitMask(pin))
+
+#define XSEG_PIN_LOW(pin)         \
+    (*portOutputRegister(digitalPinToPort(pin)) &= ~digitalPinToBitMask(pin))
+
+#define XSEG_PIN_OUTPUT(pin)      \
+    (*portModeRegister(digitalPinToPort(pin)) |= digitalPinToBitMask(pin))
+
+#define XSEG_PIN_READ(pin)        \
+    (*portInputRegister(digitalPinToPort(pin)) & digitalPinToBitMask(pin))
+
+#define XSEG_PIN_TOGGLE(pin)      \
+    (*portInputRegister(digitalPinToPort(pin)) |= digitalPinToBitMask(pin))
+
+#define XSEG_PORT_WRITE(port, mask) \
+    (port = mask)
